5b4a9ad7e821ab9ac5f9ac35649ed56d4f289e49
donskifarrell <donskifarrell+git@googlemail.com>
2013-12-15 23:14:41 +0000

    tweaked styles for shop page

--- page-shop.php
+++ page-shop.php
@@ -6,26 +6,37 @@
  */
 
 get_header(); ?>
-
-<div class="row">
-	<div class="large-12 columns">
  
-	<div class="show-for-small">	
-		<h1 class="entry-title">Shop</h1>
+
+	<div class="row shop-title">
+		<div class="large-12 columns">
+			<div class="show-for-small">	
+				<h1 class="entry-title">Shop</h1>
+			</div>
+			<h4>Click the purchase button to access the CapsLock Notification installation file.</h4>
+		</div>
 	</div>
 
- 	<div class="entry-content">
- 		<h4>Click the purchase button to access the CapsLock Notification installation file. 
- 		</h4>
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', '102Somerton' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-  
-    </div>
+	<div class="row">
+		<div class="large-8 columns">
+
+		 	<div class="entry-content">
+
+
+				<?php echo do_shortcode('[downloads number="15" columns="3"]'); ?>
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . __( 'Pages:', '102Somerton' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div>
+		</div>
+
+		<div class="verticalLine large-4 columns">
+			<h4>Your Shopping Cart</h4>
+			<?php echo do_shortcode('[download_cart]'); ?>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
