5bbb8aa4e33f3c5e16dd04882af8374b57782607
donskifarrell <donskifarrell+git@googlemail.com>
2013-12-26 15:20:55 +0000

    Tweaked checkout/payment pages

--- content-page.php
+++ content-page.php
@@ -5,15 +5,22 @@
  * @package 102Somerton
  */
 ?>
-
-	<h1 class="entry-title"><?php the_title(); ?>-COntent-Page</h1>
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', '102Somerton' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div>
 	
+	<div class="large-12 small-12 columns">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</div>
+</div>
+
+<div class="row">
+		<div class="large-9 small-12 columns">
+		 	<div class="entry-content">
+				<?php the_content(); ?>
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . __( 'Pages:', '102Somerton' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div>
+		</div>
+</div>
